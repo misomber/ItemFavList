7b9558806d641d09343b68f38774680869d1955c
Ryo <ryo.shinkai1991@gmail.com>
2018-05-19 02:35:49 +0900

    fav

--- app/AppModule.scala
+++ app/AppModule.scala
@@ -6,6 +6,7 @@ class AppModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[UserService]).to(classOf[UserServiceImpl])
     bind(classOf[ItemService]).to(classOf[ItemServiceImpl])
+    bind(classOf[ItemUserService]).to(classOf[ItemUserServiceImpl])
   }
 
 }
